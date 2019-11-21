4f2e8710e8dd2637de29bc35a724b43b4e1be45d
ReactiveWorks (Mac-mini) <macuser@ReactiveWorkss-Mac-mini-2.local>
2019-11-21 15:58:41 +0530

    Design changed to match with Platypus design

--- MyRideDNA/src/containers/chats/chat-list/index.js
+++ MyRideDNA/src/containers/chats/chat-list/index.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, TextInput, Animated, Text, Alert, Keyboard, FlatList, View, ImageBackground, ActivityIndicator, StatusBar, Easing } from 'react-native';
+import { StyleSheet, Animated, FlatList, View, ImageBackground, StatusBar, Easing } from 'react-native';
 import { IconButton, LinkButton } from '../../../components/buttons';
-import { widthPercentageToDP, heightPercentageToDP, PageKeys, APP_COMMON_STYLES, IS_ANDROID } from '../../../constants';
-import { ListItem, Left, Thumbnail, Body, Right, Icon as NBIcon, CheckBox, Toast, Item } from 'native-base';
+import { widthPercentageToDP, heightPercentageToDP, PageKeys, APP_COMMON_STYLES, CUSTOM_FONTS } from '../../../constants';
+import { ListItem, Left, Thumbnail } from 'native-base';
 import { BasicHeader } from '../../../components/headers';
 import { ShifterButton } from '../../../components/buttons';
 import { getAllChats, getPictureList, logoutUser } from '../../../api';
 import { appNavMenuVisibilityAction, updateChatPicAction, updateGroupChatPicAction, clearChatListAction } from '../../../actions';
-import { getFormattedDateFromISO } from '../../../util';
 import { Actions } from 'react-native-router-flux';
 import { BaseModal } from '../../../components/modal';
-const THUMBNAIL_SIZE = IS_ANDROID ? heightPercentageToDP(9) : heightPercentageToDP(10.5);
-const CREATE_GROUP_WIDTH = widthPercentageToDP(9);
+import { DefaultText } from '../../../components/labels';
 class ChatList extends Component {
     CHAT_LIST_OPTIONS = [{ text: 'Delete Chat', id: 'clearAll', handler: () => this.deleteFromChatList() }, { text: 'Close', id: 'close', handler: () => this.onCancelOptionsModal() }];
     frndChatPicLoading = false;
@@ -33,8 +31,6 @@ class ChatList extends Component {
         this.frndChatPicLoading = false;
         this.props.getAllChats(this.props.user.userId);
     }
-
-
 
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.chatList !== this.props.chatList) {
@@ -62,8 +58,8 @@ class ChatList extends Component {
                 this.props.getGroupMembersPic(grpPicObj);
             }
         }
-
     }
+
     retryApiFunction = () => {
         this.state.spinValue.setValue(0);
         Animated.timing(this.state.spinValue, {
@@ -76,13 +72,11 @@ class ChatList extends Component {
                 this.props.getAllChats(this.props.user.userId);
             }
         });
-
     }
 
     onPressLogout = async () => {
         this.props.logoutUser(this.props.user.userId, this.props.userAuthToken, this.props.deviceToken);
     }
-
 
     componentWillUnmount() {
         this.props.clearChatList();
@@ -104,7 +98,6 @@ class ChatList extends Component {
         else {
             return new Date(item.date).toLocaleDateString('en-IN', { day: 'numeric', year: '2-digit', month: 'short' })
         }
-
     }
 
     getFormattedTime = (dateTime) => {
@@ -116,7 +109,9 @@ class ChatList extends Component {
         return `${time.join(':')} ${period}`;
         // return new Date(dateTime).toLocaleTimeString('en-US', { hour12: true, hour: '2-digit', minute: '2-digit' });
     }
+    
     showAppNavigation = () => this.props.showAppNavMenu();
+    
     goToChatPage = (item) => {
         if (item.isGroup) {
             Actions.push(PageKeys.CHAT, { comingFrom: PageKeys.CHAT_LIST, isGroup: true, chatInfo: item })
@@ -126,44 +121,43 @@ class ChatList extends Component {
         }
     }
 
-    renderGroupName = (item) => {
-        const groupNameLength = item.groupName.length;
-        if (groupNameLength > 26) return <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#1D527C' }}>{item.groupName.substring(0, 17) + '...'}</Text>;
-        else {
-            const spacesLeft = 26 - groupNameLength - 6;
-            const firstNameStr = item.memberNameList.reduce((arr, name) => {
-                arr.push(name.split(' ')[0]);
-                return arr;
-            }, []).join(',');
-            return firstNameStr.length > spacesLeft
-                ? <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#1D527C' }}>{item.groupName}
-                    <Text style={{ color: '#585756', fontSize: 13 }}>{`  (${firstNameStr.substring(0, spacesLeft)}...)`}</Text>
-                </Text>
-                : <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#1D527C' }}>{item.groupName}
-                    {
-                        firstNameStr ? <Text style={{ color: '#585756', fontSize: 13 }}>{`  (${firstNameStr})`}</Text> : null
-                    }
-                </Text>
-        }
+    renderItemHeader = (item) => {
+        const firstNameStr = item.memberNameList.reduce((arr, name) => {
+            arr.push(name.split(' ')[0]);
+            return arr;
+        }, []).join(', ');
+        return <View style={{ flexDirection: 'row' }}>
+            <DefaultText numberOfLines={1} style={{ flex: 1, fontSize: 14, color: '#1D527C', fontFamily: CUSTOM_FONTS.robotoBold, marginRight: 10 }}>{item.groupName}
+                {
+                    firstNameStr ? <DefaultText numberOfLines={1} style={{ color: '#585756', fontSize: 13 }}>{` - ${firstNameStr}`}</DefaultText> : null
+                }
+            </DefaultText>
+            <DefaultText style={{ color: '#8D8D8D', letterSpacing: 0.8, fontSize: 10, fontFamily: CUSTOM_FONTS.robotoSlabBold }}>{item.message ? this.getFormattedTime(item.message.date) : null}</DefaultText>
+        </View>
+    }
+
+    renderItemBodyContent(item) {
+        return <View style={styles.bodyCont}>
+            {
+                item.isGroup
+                    ? this.renderItemHeader(item)
+                    : <DefaultText style={{ fontSize: 14, color: '#1D527C', fontFamily: CUSTOM_FONTS.robotoBold }}>{item.name}</DefaultText>
+            }
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+                <DefaultText numberOfLines={2} style={{ marginTop: 5, marginRight: 5, color: '#585756', flex: 1 }}>{item.message ? item.message.content : null}</DefaultText>
+                {
+                    item.totalUnseenMessage > 0
+                        ? <View style={styles.messageNumber}>
+                            <DefaultText style={{ alignSelf: 'center', color: '#FFFFFF' }}>{'99+'}</DefaultText>
+                        </View>
+                        : null
+                }
+            </View>
+        </View>
     }
 
     renderChatList = ({ item, index }) => {
-        // if (item.isGroup) {
-        //     return (
-        //         <ListItem style={{ marginTop: 20 }} avatar onLongPress={() => console.log('onLongPressChatList')} onPress={() => console.log('onPressChatLIst')}>
-        //             <Left style={{ alignItems: 'center', justifyContent: 'center' }}>
-        //                 <Thumbnail style={styles.thumbnail} source={require('../../../assets/img/friend-profile-pic.png')} />
-        //             </Left>
-        //             <Body>
-
-        //             </Body>
-        //             <Right>
-        //             </Right>
-        //         </ListItem>
-        //     );
-        // }
         return (
-            // <ListItem avatar style={{}} onPress={() => this.goToChatPage(item)} onLongPress={()=>this.showOptionsModal()}>
             <ListItem noIndent style={styles.itemCont} onPress={() => this.goToChatPage(item)}>
                 {
                     item.isGroup
@@ -178,14 +172,7 @@ class ChatList extends Component {
                                         </View>
                                         : <IconButton disabled style={[styles.iconContComm, styles.groupIconCont]} iconProps={{ name: 'users', type: 'FontAwesome', style: styles.iconComm }} />
                             }
-                            <View style={styles.bodyCont}>
-                                {
-                                    item.isGroup
-                                        ? this.renderGroupName(item)
-                                        : <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#1D527C' }}>{item.name}</Text>
-                                }
-                                <Text style={{ fontSize: 12, marginTop: 5, color: '#585756' }}>{item.message ? item.message.content.length > 25 ? item.message.content.substring(0, 26) + '...' : item.message.content : null}</Text>
-                            </View>
+                            {this.renderItemBodyContent(item)}
                         </Left>
                         : <Left style={styles.leftCont}>
                             {
@@ -193,25 +180,9 @@ class ChatList extends Component {
                                     ? <Thumbnail style={styles.iconContComm} source={{ uri: item.profilePicture }} />
                                     : <IconButton disabled style={[styles.iconContComm, styles.userIconCont]} iconProps={{ name: 'user', type: 'FontAwesome', style: styles.iconComm }} />
                             }
-                            <View style={styles.bodyCont}>
-                                {
-                                    item.isGroup
-                                        ? this.renderGroupName(item)
-                                        : <Text style={{ fontSize: 14, fontWeight: 'bold', color: '#1D527C' }}>{item.name}</Text>
-                                }
-                                <Text style={{ fontSize: 12, marginTop: 5, color: '#585756' }}>{item.message ? item.message.content.length > 25 ? item.message.content.substring(0, 26) + '...' : item.message.content : null}</Text>
-                            </View>
+                            {this.renderItemBodyContent(item)}
                         </Left>
                 }
-                <Right style={styles.rightCont}>
-                    <Text style={{ color: '#8D8D8D', letterSpacing: 0.8, fontSize: 11 }}>{item.message ? this.getFormattedTime(item.message.date) : null}</Text>
-                    {
-                        item.totalUnseenMessage > 0 ?
-                            <View style={styles.messageNumber}>
-                                <Text style={{ alignSelf: 'center', color: '#FFFFFF', fontSize: heightPercentageToDP(2) }}>{item.totalUnseenMessage > 99 ? '99+' : item.totalUnseenMessage}</Text>
-                            </View>
-                            : null}
-                </Right>
             </ListItem>
         );
 
@@ -221,6 +192,7 @@ class ChatList extends Component {
     chatListKeyExtractor = (item) => item.id;
 
     onCancelOptionsModal = () => this.setState({ isVisibleOptionsModal: false })
+
     renderMenuOptions = () => {
         let options = null;
         options = this.CHAT_LIST_OPTIONS;
@@ -275,8 +247,8 @@ class ChatList extends Component {
                                         <Animated.View style={{ transform: [{ rotate: spin }] }}>
                                             <IconButton iconProps={{ name: 'reload', type: 'MaterialCommunityIcons', style: { color: 'black', width: widthPercentageToDP(13), fontSize: heightPercentageToDP(15), flex: 1, marginLeft: widthPercentageToDP(40) } }} onPress={this.retryApiFunction} />
                                         </Animated.View>
-                                        <Text style={{ marginLeft: widthPercentageToDP(13), fontSize: heightPercentageToDP(4.5) }}>No Internet Connection</Text>
-                                        <Text style={{ marginTop: heightPercentageToDP(2), marginLeft: widthPercentageToDP(25) }}>Please connect to internet </Text>
+                                        <DefaultText style={{ marginLeft: widthPercentageToDP(13), fontSize: heightPercentageToDP(4.5) }}>No Internet Connection</DefaultText>
+                                        <DefaultText style={{ marginTop: heightPercentageToDP(2), marginLeft: widthPercentageToDP(25) }}>Please connect to internet </DefaultText>
                                     </View>
                         }
                     </View>
@@ -352,11 +324,11 @@ const styles = StyleSheet.create({
         paddingTop: 10
     },
     bodyCont: {
+        marginLeft: 5,
         height: 68,
         paddingTop: 5,
         flexDirection: 'column',
-        flex: 1,
-        marginLeft: 5
+        flex: 1
     },
     backgroundImage: {
         height: null,
@@ -397,7 +369,7 @@ const styles = StyleSheet.create({
         // marginTop: heightPercentageToDP(1.4),
         height: 30,
         minWidth: 30,
-        borderRadius: 15,
+        borderRadius: 30,
         justifyContent: 'center',
         alignItems: 'center'
     },
