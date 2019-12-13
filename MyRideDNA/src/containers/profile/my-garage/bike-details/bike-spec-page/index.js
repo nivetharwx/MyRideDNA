304c03b5c0f3f4a388db6e1c9fc6765aa0d371a5
ReactiveWorks (Mac-mini) <macuser@ReactiveWorkss-Mac-mini-2.local>
2019-12-13 16:51:01 +0530

    - Changed design - Integrated API for delete

--- MyRideDNA/src/containers/profile/my-garage/bike-details/bike-spec-page/index.js
+++ MyRideDNA/src/containers/profile/my-garage/bike-details/bike-spec-page/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { View, ImageBackground, StatusBar, FlatList, StyleSheet, ActivityIndicator, Animated } from 'react-native';
+import { Alert, View, ImageBackground, Image, StatusBar, FlatList, StyleSheet, ActivityIndicator, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { IconButton, LinkButton } from '../../../../../components/buttons';
-import { APP_COMMON_STYLES, widthPercentageToDP, heightPercentageToDP, PageKeys, THUMBNAIL_TAIL_TAG, MEDIUM_TAIL_TAG, POST_TYPE, PORTRAIT_TAIL_TAG, GET_PICTURE_BY_ID } from '../../../../../constants';
+import { APP_COMMON_STYLES, widthPercentageToDP, heightPercentageToDP, PageKeys, THUMBNAIL_TAIL_TAG, MEDIUM_TAIL_TAG, POST_TYPE, PORTRAIT_TAIL_TAG, GET_PICTURE_BY_ID, CUSTOM_FONTS } from '../../../../../constants';
 import { BaseModal } from '../../../../../components/modal';
 import { Actions } from 'react-native-router-flux';
 import { BasicHeader } from '../../../../../components/headers';
 import { SquareCard } from '../../../../../components/cards';
 import { DefaultText } from '../../../../../components/labels';
-import { appNavMenuVisibilityAction } from '../../../../../actions';
-import { getPosts } from '../../../../../api';
+import { appNavMenuVisibilityAction, updatePageContentStatusAction } from '../../../../../actions';
+import { getPosts, deletePost } from '../../../../../api';
 
 class BikeSpec extends Component {
     constructor(props) {
@@ -33,7 +33,23 @@ class BikeSpec extends Component {
 
     openPostForm = () => Actions.push(PageKeys.POST_FORM, { comingFrom: Actions.currentScene, postType: this.props.postType, currentBikeId: this.props.bike.spaceId });
 
-    onPressDeleteSpec = () => { }
+    onPressDeleteSpec = () => {
+        setTimeout(() => {
+            Alert.alert(
+                'Delete confirmation',
+                `Are you sure to delete ${this.props.specification.title}?`,
+                [
+                    {
+                        text: 'Cancel',
+                        onPress: () => null,
+                        style: 'cancel',
+                    },
+                    { text: 'Delete', onPress: () => this.props.deleteSpec(this.props.postType, this.props.specification.id, () => Actions.pop()) },
+                ]
+            );
+        }, 100);
+        this.hideOptionsModal();
+    }
 
     render() {
         const { showOptionsModal } = this.state;
@@ -54,9 +70,22 @@ class BikeSpec extends Component {
                     leftIconProps={{ reverse: true, name: 'md-arrow-round-back', type: 'Ionicons', onPress: this.onPressBackButton }}
                     rightIconProps={{ name: 'options', type: 'SimpleLineIcons', style: { color: '#fff', fontSize: 20 }, onPress: this.showOptionsModal }}
                 />
-                <View style={styles.pageContent}>
-                    
-                </View>
+                <ScrollView
+                    style={styles.pageContent}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingTop: APP_COMMON_STYLES.headerHeight }}
+                >
+                    <View style={styles.profilePic}>
+                        <ImageBackground source={specification.pictureIds && specification.pictureIds[0] ? { uri: `${GET_PICTURE_BY_ID}${specification.pictureIds[0].id.replace(THUMBNAIL_TAIL_TAG, MEDIUM_TAIL_TAG)}` } : require('../../../../../assets/img/profile-pic.png')} style={{ height: null, width: null, flex: 1, borderRadius: 0 }} />
+                    </View>
+                    <Image source={require('../../../../../assets/img/profile-bg.png')} style={styles.profilePicBtmBorder} />
+                    <View style={styles.pageContent}>
+                        <View style={styles.contentPadding}>
+                            <DefaultText style={styles.labelText}>ADDITIONAL INFO</DefaultText>
+                            <DefaultText style={styles.labelContent}>{specification.description || ""}</DefaultText>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         </View>
     }
@@ -71,6 +100,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         showAppNavMenu: () => dispatch(appNavMenuVisibilityAction(true)),
+        deleteSpec: (postType, postId, successCallback) => dispatch(deletePost(postId, () => {
+            dispatch(updatePageContentStatusAction({ type: postType, action: 'DELETE' }));
+            typeof successCallback === 'function' && successCallback();
+        })),
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(BikeSpec);
@@ -83,4 +116,26 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
+    contentPadding: {
+        marginHorizontal: 27,
+        marginTop: 20
+    },
+    profilePic: {
+        width: widthPercentageToDP(100),
+        height: 255,
+    },
+    profilePicBtmBorder: {
+        width: widthPercentageToDP(100),
+        height: 13
+    },
+    labelText: {
+        color: '#707070',
+        fontFamily: CUSTOM_FONTS.robotoSlabBold,
+        letterSpacing: 1.6,
+        fontSize: 8
+    },
+    labelContent: {
+        marginTop: 10,
+        fontFamily: CUSTOM_FONTS.robotoSlabBold,
+    }
 });
